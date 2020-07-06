7d97f61f3a84385e28f5ceeddb2d87327cb2efd0
Melinn Phifer <mphifer@ufl.edu>
2020-07-06 16:14:39 -0400

    add google fonts to default.php

--- form/default.inc.php
+++ form/default.inc.php
@@ -464,6 +464,8 @@ padding-bottom: 1%;
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@600&family=Poppins:wght@300;600&display=swap" rel="stylesheet">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <div id="container">
